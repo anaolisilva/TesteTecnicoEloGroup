6cfc45f2cc8a7709bbb03894d2e141185cabd809
Ana Oliveira <vianello.ana@gmail.com>
2021-08-26 16:46:40 -0300

    Melhorias e observações em novo lead e login

--- src/app/novolead/novolead.component.ts
+++ src/app/novolead/novolead.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormsModule, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormsModule, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-novolead',
@@ -25,41 +25,61 @@ export class NovoleadComponent implements OnInit {
 
 
   ngOnInit() {
-    console.log(this.formularioLead)
+    console.log(this.formularioLead.controls.oportunidades.getError('minimo'))
   }
 
+  //Construção do array de oportunidades
   buildOportunidades() {
 
     const oportunidadesEscolhidas = this.oportunidades.map(op => new FormControl(false));
 
-    return this.formBuilder.array(oportunidadesEscolhidas);
-
+    return this.formBuilder.array(oportunidadesEscolhidas, this.requiredMinCheckbox);
   }
 
-  salvar() {
-    let oportunidadesSubmit = Object.assign({}, this.formularioLead.value);
+  //Regra de validação dos Checkbox
+  //Passar todas as regras de validação para uma service de formulários?
+  requiredMinCheckbox(){
+    const validator = (op: FormArray) => {
+      const totalChcked = op.controls.map(v => v.value).reduce((total, atual) => atual ? total + atual : total, 0);
+      return totalChcked >= 1 ? null : {minimo : true};
 
-    oportunidadesSubmit = Object.assign(oportunidadesSubmit, {
-      oportunidades: oportunidadesSubmit.oportunidades
+    }
+
+    return validator;
+  }
+
+  //Função do botão salvar
+  salvar() {
+    // let oportunidadesSubmit = Object.assign({}, this.formularioLead.value);
+
+    let oportunidadesSubmit = Object.assign(this.formularioLead.value, {
+      oportunidades: this.formularioLead.value.oportunidades
         .map((op: boolean, i: number) => op ? this.oportunidades[i] : null)
         .filter((op: string) => op != null)
     });
 
-    console.log(oportunidadesSubmit, this.formularioLead)
+    console.log(oportunidadesSubmit)
+
+
+    if (oportunidadesSubmit.oportunidades.length == 0) {
+      alert('Escolha pelo menos uma oportunidade.')
+    }
+
 
     //Colocar o dado do nome do lead no localstorage provavelmente
-    //Talvez seja interessante, se der tempo, colocar validação de pelo menos um nos checkboxes
+    //Colocar validação de pelo menos um nos checkboxes
 
 }
 
 // Função que marca/desmarca todos os checkbox.
-
+// Não funciona porque não as adiciona no array.
   checkAll(){
     if (!this.todosSelecionados){
       this.todosSelecionados = true;
     } else {
       this.todosSelecionados = false;
     }
+
   }
 
 }
